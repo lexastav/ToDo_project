5be48e157b54ff63c30a1c6b0cfaec0acd1614b3
Alexey Volegov <lexastav@me.com>
2021-05-05 13:18:38 +0000

    Add in production

--- ToDo_Project/settings.py
+++ ToDo_Project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#sz07j+aau&r0-j-41co5l-a%yal*&q=*rzpbe&hm5y44=w)bt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lexastav.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
